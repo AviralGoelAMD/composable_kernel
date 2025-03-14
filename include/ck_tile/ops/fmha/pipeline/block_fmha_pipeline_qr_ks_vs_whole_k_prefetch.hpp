884c5b1470694a002145fc505b4d4cfdf3b25085
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2025-03-14 10:13:08 +0000

    Remove constexpr(k1_loops > 1) checking

--- include/ck_tile/ops/fmha/pipeline/block_fmha_pipeline_qr_ks_vs_whole_k_prefetch.hpp
+++ include/ck_tile/ops/fmha/pipeline/block_fmha_pipeline_qr_ks_vs_whole_k_prefetch.hpp
@@ -752,72 +752,67 @@ struct BlockFmhaPipelineQRKSVSWholeKPrefetch
             }
 
             // STAGE 3, KV gemm
-            if constexpr(k1_loops > 1)
+            if constexpr(NumPrefetchV == 1) // NumVLdsBuffers == 2
             {
-                if constexpr(NumPrefetchV == 1) // NumVLdsBuffers == 2
-                {
-                    static_for<0, k1_loops - 1, 1>{}([&](auto i_k1) {
-                        v_tiles[I0] = load_tile(v_dram_window);
+                static_for<0, k1_loops - 1, 1>{}([&](auto i_k1) {
+                    v_tiles[I0] = load_tile(v_dram_window);
 
-                        block_sync_lds();
-                        gemm_1(o_acc,
-                               get_slice_tile(
-                                   p, sequence<0, i_k1 * kK1>{}, sequence<kM0, (i_k1 + 1) * kK1>{}),
-                               v_lds_windows[number<i_k1 % NumVLdsBuffers>{}]);
+                    block_sync_lds();
+                    gemm_1(o_acc,
+                           get_slice_tile(
+                               p, sequence<0, i_k1 * kK1>{}, sequence<kM0, (i_k1 + 1) * kK1>{}),
+                           v_lds_windows[number<i_k1 % NumVLdsBuffers>{}]);
 
-                        if constexpr(std::is_same_v<VLayout,
-                                                    ck_tile::tensor_layout::gemm::RowMajor>)
-                        {
-                            auto v_shuffle_tmp = make_static_distributed_tensor<VDataType>(
-                                Policy::template MakeShuffledVRegBlockDescriptor<Problem>());
-                            shuffle_tile(v_shuffle_tmp, v_tiles[I0]);
-                            store_tile(v_lds_windows[number<(i_k1 + 1) % NumVLdsBuffers>{}],
-                                       tile_elementwise_in(v_element_func, v_shuffle_tmp));
-                        }
-                        else
-                        {
-                            store_tile(v_lds_windows[number<(i_k1 + 1) % NumVLdsBuffers>{}],
-                                       tile_elementwise_in(v_element_func, v_tiles[I0]));
-                        }
+                    if constexpr(std::is_same_v<VLayout, ck_tile::tensor_layout::gemm::RowMajor>)
+                    {
+                        auto v_shuffle_tmp = make_static_distributed_tensor<VDataType>(
+                            Policy::template MakeShuffledVRegBlockDescriptor<Problem>());
+                        shuffle_tile(v_shuffle_tmp, v_tiles[I0]);
+                        store_tile(v_lds_windows[number<(i_k1 + 1) % NumVLdsBuffers>{}],
+                                   tile_elementwise_in(v_element_func, v_shuffle_tmp));
+                    }
+                    else
+                    {
+                        store_tile(v_lds_windows[number<(i_k1 + 1) % NumVLdsBuffers>{}],
+                                   tile_elementwise_in(v_element_func, v_tiles[I0]));
+                    }
 
-                        move_tile_window(v_dram_window, {0, kK1});
-                    });
-                }
-                else // NumVLdsBuffers == 3 or 2
-                {
-                    static_for<0, k1_loops - 1, 1>{}([&](auto i_k1) {
-                        if constexpr(i_k1 < k1_loops - NumPrefetchV)
-                            v_tiles[number<i_k1 % NumPrefetchV>{}] = load_tile(v_dram_window);
-
-                        block_sync_lds();
-                        gemm_1(o_acc,
-                               get_slice_tile(
-                                   p, sequence<0, i_k1 * kK1>{}, sequence<kM0, (i_k1 + 1) * kK1>{}),
-                               v_lds_windows[number<i_k1 % NumVLdsBuffers>{}]);
-
-                        if constexpr(std::is_same_v<VLayout,
-                                                    ck_tile::tensor_layout::gemm::RowMajor>)
-                        {
-                            auto v_shuffle_tmp = make_static_distributed_tensor<VDataType>(
-                                Policy::template MakeShuffledVRegBlockDescriptor<Problem>());
-                            shuffle_tile(v_shuffle_tmp,
-                                         v_tiles[number<(i_k1 + 1) % NumPrefetchV>{}]);
-                            store_tile(v_lds_windows[number<(i_k1 + 1) % NumVLdsBuffers>{}],
-                                       tile_elementwise_in(v_element_func, v_shuffle_tmp));
-                        }
-                        else
-                        {
-                            store_tile(
-                                v_lds_windows[number<(i_k1 + 1) % NumVLdsBuffers>{}],
-                                tile_elementwise_in(v_element_func,
-                                                    v_tiles[number<(i_k1 + 1) % NumPrefetchV>{}]));
-                        }
-
-                        if constexpr(i_k1 < k1_loops - NumPrefetchV)
-                            move_tile_window(v_dram_window, {0, kK1});
-                    });
-                }
+                    move_tile_window(v_dram_window, {0, kK1});
+                });
             }
+            else // NumVLdsBuffers == 3 or 2
+            {
+                static_for<0, k1_loops - 1, 1>{}([&](auto i_k1) {
+                    if constexpr(i_k1 < k1_loops - NumPrefetchV)
+                        v_tiles[number<i_k1 % NumPrefetchV>{}] = load_tile(v_dram_window);
+
+                    block_sync_lds();
+                    gemm_1(o_acc,
+                           get_slice_tile(
+                               p, sequence<0, i_k1 * kK1>{}, sequence<kM0, (i_k1 + 1) * kK1>{}),
+                           v_lds_windows[number<i_k1 % NumVLdsBuffers>{}]);
+
+                    if constexpr(std::is_same_v<VLayout, ck_tile::tensor_layout::gemm::RowMajor>)
+                    {
+                        auto v_shuffle_tmp = make_static_distributed_tensor<VDataType>(
+                            Policy::template MakeShuffledVRegBlockDescriptor<Problem>());
+                        shuffle_tile(v_shuffle_tmp, v_tiles[number<(i_k1 + 1) % NumPrefetchV>{}]);
+                        store_tile(v_lds_windows[number<(i_k1 + 1) % NumVLdsBuffers>{}],
+                                   tile_elementwise_in(v_element_func, v_shuffle_tmp));
+                    }
+                    else
+                    {
+                        store_tile(
+                            v_lds_windows[number<(i_k1 + 1) % NumVLdsBuffers>{}],
+                            tile_elementwise_in(v_element_func,
+                                                v_tiles[number<(i_k1 + 1) % NumPrefetchV>{}]));
+                    }
+
+                    if constexpr(i_k1 < k1_loops - NumPrefetchV)
+                        move_tile_window(v_dram_window, {0, kK1});
+                });
+            }
+
             // move K tile windows
             move_tile_window(k_dram_block_window, {kN0, 0});
 
