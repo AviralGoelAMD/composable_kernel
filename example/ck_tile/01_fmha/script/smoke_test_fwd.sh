d7618c386b3b3ae443d2d6f0f5daeeee2395107b
Anton Gorenko <anton@streamhpc.com>
2025-06-16 14:32:08 +0500

    Pass hdim to tile_example_fmha_fwd in fp8 tests

--- example/ck_tile/01_fmha/script/smoke_test_fwd.sh
+++ example/ck_tile/01_fmha/script/smoke_test_fwd.sh
@@ -72,8 +72,8 @@ run_fp8_tests() {
     for bias in "n" "e" "a" ; do
     for b in 1 2 ; do
     for hdim in 64 128 256 ; do
-    
-    $EXE -prec=fp8 -init=3 -b=$b -h=1 -d=128 -s=128 -bias=$bias -iperm=$perm -operm=$perm -vlayout=c -squant=1 -kname=$KNAME $COMMON_ARGS  
+
+    $EXE -prec=fp8 -init=3 -b=$b -h=1 -d=$hdim -s=128 -bias=$bias -iperm=$perm -operm=$perm -vlayout=c -squant=1 -kname=$KNAME $COMMON_ARGS  
 
     done ; done ; done ; done
 }
