326fdb79024a8388c01a32eedfed620713c190f9
AviralGoelAMD <aviral.goel@amd.com>
2025-07-23 22:38:32 +0000

    added code to help profile

--- example/ck_tile/18_flatmm/flatmm_basic.cpp
+++ example/ck_tile/18_flatmm/flatmm_basic.cpp
@@ -99,6 +99,8 @@ float flatmm_calc(const ck_tile::FlatmmHostArgs& args, const ck_tile::stream_con
         using CodegenFlatmmPolicy = ck_tile::UniversalFlatmmPipelineAgBgCrPolicy;
         using CodegenFlatmmPipeline =
             ck_tile::FlatmmPipelineAGmemBGmemCRegV0<CodegenPipelineProblem, CodegenFlatmmPolicy>;
+        
+        std::cout << "CodegenFlatmmPipeline: " << CodegenFlatmmPipeline::GetName() << std::endl;
 
         // ToDo: Will add the codegen part to test different pipeline policies in GEMM.
         // Now we only use the BlockGemmASmemBSmemCRegV1DefaultPolicy.
