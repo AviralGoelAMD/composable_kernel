fa3b366716cc373e0183a5ffd5b050b8961dc0c9
ThomasNing <thomasning@amd.com>
2025-08-04 20:31:55 -0500

    Fix and debug the pass through feature

--- tile_engine/ops/gemm_multi_d/gemm_multi_d_codegen_utils.py
+++ tile_engine/ops/gemm_multi_d/gemm_multi_d_codegen_utils.py
@@ -55,7 +55,7 @@ CSHUFFLE_EPILOGUE = """
                                                              EDataType,
                                                              DsLayout,
                                                              ELayout,
-                                                             ck_tile::element_wise::PassThrough,
+                                                             CDElementwise,
                                                              GemmPipelineProblem::kBlockSize,
                                                              TilePartitioner::MPerBlock,
                                                              TilePartitioner::NPerBlock,
